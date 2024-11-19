855b18064fb1261b6e1e9f0ceb7cb20ed82e3d0a
Gowtham <149290319+GowthamProg@users.noreply.github.com>
2024-11-19 18:50:27 +0530

    Time: 1 ms (100%), Space: 57.6 MB (71.43%) - LeetHub

---
+++ 3248-count-the-number-of-incremovable-subarrays-ii/3248-count-the-number-of-incremovable-subarrays-ii.java
@@ -0,0 +1,45 @@
+class Solution {
+    public long incremovableSubarrayCount(int[] nums) {
+        int n = nums.length;
+        
+        int leftIncreasing = 1;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > nums[i - 1]) {
+                leftIncreasing++;
+            } else {
+                break;
+            }
+        }
+
+        // simplier to just return the answer when the whole nums is a strictly increasing sequence
+        if (leftIncreasing == n) {
+            return (1L + (long)n) * (long)n / 2L;
+        }
+
+        int rightIncreasing = 1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i + 1] > nums[i]) {
+                rightIncreasing++;
+            } else {
+                break;
+            }
+        }
+        
+        // find the patten of removing a subarray starts from i != 0 to i != n - 1
+        long twinIncreasing = 0;
+        int r = n - rightIncreasing;
+        for (int l = 0; l < leftIncreasing; l++) {
+            while (r < n && nums[l] >= nums[r]) {
+                r++;
+            }
+            if (r == n) {
+                break;
+            }
+            twinIncreasing += (long)(n - r);
+        }
+
+        // remember to add the case removing all numbers
+        return (1L + (long)leftIncreasing + (long)rightIncreasing + (long)twinIncreasing);
+        
+    }
+}
