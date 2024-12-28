8e2c1a967b2779ef7a2a365c1f1c3dbbab49b11d
Gowtham <149290319+GowthamProg@users.noreply.github.com>
2024-12-28 16:13:52 +0530

    Time: 0 ms (100%), Space: 8 MB (40.17%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.c
+++ 0162-find-peak-element/0162-find-peak-element.c
@@ -1,6 +1,18 @@
-int findPeakElement(int*a, int n) {
-    for(int i=0;i<n-1;i++) 
-        if(a[i]>a[i+1]) 
-            return i;
-    return n-1;
+// int findPeakElement(int*a, int n) {
+//     for(int i=0;i<n-1;i++) 
+//         if(a[i]>a[i+1]) 
+//             return i;
+//     return n-1;
+// }
+
+int findPeakElement(int*a, int n)
+{
+    int left=0,right=n-1;
+    while(left<right)
+    {
+        int mid=(left+right)/2;
+        if(a[mid]>a[mid+1]) right=mid;
+        else left=mid+1;
+    }
+    return left;
 }
