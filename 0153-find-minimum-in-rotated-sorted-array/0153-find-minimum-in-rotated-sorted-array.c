a038d6a061d9a842b5dbd9cc17e229c085f6ed86
Gowtham <149290319+GowthamProg@users.noreply.github.com>
2024-12-28 14:35:15 +0530

    Time: 0 ms (100%), Space: 8.4 MB (27.22%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.c
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.c
@@ -6,17 +6,36 @@
 // }
 
 
-int findMin(int* num, int nS) {
-        int right=nS-1,left=0,mid;
-        if(num[0]<num[nS-1])return num[0];
-        while(left<right)
-        {
-            mid = (left + right)/2;
-            if(num[mid]>num[mid+1])return num[mid+1];
-            if(num[mid-1]>num[mid])return num[mid];
-            if(num[mid]>num[0]) {left=mid+1;}
-            else if(num[mid]<num[0]){right=mid+1;}
-        }
-        return num[mid];
-
+int findMin(int* a, int n)
+{
+    int left=0,right=n-1;
+    if(a[0]<a[right]) return a[0];
+    while(left<=right)
+    {
+        int mid=(right+left)/2;
+        if(a[mid]>a[n-1])left= mid+1;
+        else  right=mid-1;
+    }
+    return a[left];
 }
+
+
+
+
+
+
+
+// int findMin(int* num, int nS) {
+//         int right=nS-1,left=0,mid;
+//         if(num[0]<num[nS-1])return num[0];
+//         while(left<right)
+//         {
+//             mid = (left + right)/2;
+//             if(num[mid]>num[mid+1])return num[mid+1];
+//             if(num[mid-1]>num[mid])return num[mid];
+//             if(num[mid]>num[0]) {left=mid+1;}
+//             else if(num[mid]<num[0]){right=mid+1;}
+//         }
+//         return num[mid];
+
+// }
