1f395e0139860bc1cf781ec0b41da031e0b9ce90
Gowtham <gowthamshan1.12@gmail.com>
2024-12-30 15:32:50 +0530

    Time: 9 ms (68.06%), Space: 11.1 MB (87.05%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.c
+++ 0141-linked-list-cycle/0141-linked-list-cycle.c
@@ -5,6 +5,21 @@
  *     struct ListNode *next;
  * };
  */
+
+bool hasCycle(struct ListNode *head)
+{
+    if(head==NULL || head->next==NULL) return 0;
+    struct ListNode *slow=head,*fast=head;
+    while(fast!=NULL && fast->next!=NULL)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast) return 1;
+    }
+    return 0;
+}
+
+/*
 bool hasCycle(struct ListNode *head) {
     if(head==NULL || head->next==NULL) return false;
     struct ListNode *fast=head,*slow=head;
@@ -16,3 +31,4 @@ bool hasCycle(struct ListNode *head) {
     }
     return 0;
 }
+*/
