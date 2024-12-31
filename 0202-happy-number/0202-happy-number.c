e121b44dbd6babea937941564cc2a380f0935430
Gowtham <gowthamshan1.12@gmail.com>
2024-12-31 09:36:35 +0530

    Time: 0 ms (100%), Space: 7.7 MB (42.16%) - LeetHub

--- 0202-happy-number/0202-happy-number.c
+++ 0202-happy-number/0202-happy-number.c
@@ -11,8 +11,30 @@ int compute(int n)
 bool isHappy(int n) {
     int slow=n,fast=n;
     do {
-        n=compute(n);
-    }while(n!=1 && n!=4);
-    if(n==1) return 1;
+        slow=compute(slow);
+        fast=compute(fast);
+        fast=compute(fast);
+    }while(slow!=fast);
+    if(slow==1) return slow;
     else return 0;
 }
+
+
+
+// int compute(int n)
+// {
+//     int res=0;
+//     while(n>0)
+//     {
+//         res=res+(n%10)*(n%10);
+//         n=n/10;
+//     }
+//     return res;
+// }
+// bool isHappy(int n) {
+//     do {
+//         n=compute(n);
+//     }while(n!=1 && n!=4);
+//     if(n==1) return 1;
+//     else return 0;
+// }
