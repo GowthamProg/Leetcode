329f3ff310e0f77de06820d0c4a960e6333808a2
Gowtham <gowthamshan1.12@gmail.com>
2025-01-03 15:22:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.c
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.c
@@ -18,7 +18,7 @@ void post(struct Node* root, int* index,int *res)
     res[(*index)++]=root->val;
 }
 int* postorder(struct Node* root, int* r) {
-    int *res=(int*)malloc(10000*sizeof(int));
+    int *res=(int*)malloc(100*sizeof(int));
     *r=0;
     post(root,r,res);
     return res;
