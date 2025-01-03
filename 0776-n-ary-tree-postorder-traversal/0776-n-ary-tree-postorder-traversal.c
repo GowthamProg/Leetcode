48fa649ad73194a31fee02b5a18c0b60121b4229
Gowtham <gowthamshan1.12@gmail.com>
2025-01-03 15:19:53 +0530

    Time: 12 ms (55.22%), Space: 13 MB (38.81%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.c
@@ -0,0 +1,25 @@
+/**
+ * Definition for a Node.
+ * struct Node {
+ *     int val;
+ *     int numChildren;
+ *     struct Node** children;
+ * };
+ */
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+void post(struct Node* root, int* index,int *res)
+{
+    if(root==NULL) return;
+    for(int i=0;i<root->numChildren;i++)
+    post(root->children[i],index,res);
+    res[(*index)++]=root->val;
+}
+int* postorder(struct Node* root, int* r) {
+    int *res=(int*)malloc(10000*sizeof(int));
+    *r=0;
+    post(root,r,res);
+    return res;
+}
