14e6edb15426d56517dbc83a8ced9631afb0337e
Gowtham M S <gowthamshan1.12@gmail.com>
2025-01-21 15:28:06 +0530

    Time: 0 ms (100%), Space: 7.7 MB (56.3%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.c
@@ -0,0 +1,14 @@
+int tribonacci(int n) {
+    if(n==0 || n==1) return n;
+    if(n==2) return 1;
+    int a=0,b=1,c=1,res=0;
+    for(int i=3;i<=n;i++)
+    {
+        res=a+b+c;
+        a=b;
+        b=c;
+        c=res;
+    }
+    printf("%d",res);
+    return res;
+}
