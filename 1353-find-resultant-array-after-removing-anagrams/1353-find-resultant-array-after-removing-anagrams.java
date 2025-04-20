aa0c7824267c20d94f6d3d90753c490cd1d7c962
Gowtham M S <gowthamshan1.12@gmail.com>
2025-04-20 18:15:06 +0530

    Time: 3 ms (53.51%), Space: 44.8 MB (59.93%) - LeetHub

--- 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.java
@@ -1,20 +1,41 @@
 class Solution {
-    public List<String> removeAnagrams(String[] words) {
-       List <String> result = new ArrayList<>();
-       String previous= sortstring(words[0]);
-       result.add(words[0]);
-       for(int i=1;i<words.length;i++){
-        String current = sortstring(words[i]);
-        if(!previous.equals(current)){
-            previous=current;
-            result.add(words[i]);
+    public List<String> removeAnagrams(String[] w) {
+        HashMap <Integer,String> map =new HashMap<>();
+        int j=0;
+        map.put(j++,w[0]);
+        String prev= sort(w[0]);
+        for(int i=1;i<w.length;i++){
+            String current=sort(w[i]);
+            if(!current.equals(prev)){
+                prev=current;
+                map.put(j++,w[i]);
+            }
         }
-       } 
-       return result;
+        return new ArrayList<>(map.values());
     }
-    private String sortstring(String s){
-        char []ch= s.toCharArray();
+    private String sort(String s){
+        char ch[]=s.toCharArray();
         Arrays.sort(ch);
         return new String(ch);
-        }
+    }
 }
+// class Solution {
+//     public List<String> removeAnagrams(String[] words) {
+//        List <String> result = new ArrayList<>();
+//        String previous= sortstring(words[0]);
+//        result.add(words[0]);
+//        for(int i=1;i<words.length;i++){
+//         String current = sortstring(words[i]);
+//         if(!previous.equals(current)){
+//             previous=current;
+//             result.add(words[i]);
+//         }
+//        } 
+//        return result;
+//     }
+//     private String sortstring(String s){
+//         char []ch= s.toCharArray();
+//         Arrays.sort(ch);
+//         return new String(ch);
+//         }
+// }
