d52b252fe8c629f8ca1318f072850ebfd861416b
Gowtham M S <gowthamshan1.12@gmail.com>
2025-04-20 19:03:50 +0530

    Time: 11 ms (9.14%), Space: 44.7 MB (21.06%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.java
@@ -2,13 +2,12 @@ class Solution {
     public int minLength(String s) {
         String result=s,current=s;
         int flag=1;
-        while(true){
+        int i=100;
+        while(i>=0){
             current=result;
             result=result.replace("AB","");
             result=result.replace("CD","");
-            if(current.equals(result)){
-                break;
-            }
+           i--;
         }
         return current.length();
     }
